1dd375917b1a2a65a5ff418ba95fae076f82b2c7
Nikolay <ceo@machineand.me>
2020-05-05 07:39:26 +0300

    Resloved language examples

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ from app import syntax, distortion
 
 import os
 
-BASE = "/Users/nikolaykiselev/Life/shirts/language-examples/languages"
+BASE = "/Users/nikolaykiselev/Life/shirts/language-examples/"
 for i in os.listdir(BASE):
     FILEPATH = i
-    with open(BASE + '/' +FILEPATH) as file:
+    with open(BASE + FILEPATH) as file:
         TEST_CODE = file.read()
     tokens = [i for i in syntax.tokenize(FILEPATH, TEST_CODE)]
     img = syntax.show_pic(iter(tokens))
